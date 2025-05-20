fbd84d99eb3237d6b5b1d0ce32eb3a1b7e8befee
natashanyokabi <natashawairimu27@gmail.com>
2025-05-20 08:38:42 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
-
-def square_integers(int_list):
-    # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    print("Happy New Year!")
